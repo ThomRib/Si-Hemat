ad3aa6abf63c60160a736b64b37561f7a7ae6d36
Thomas <74390441+ThomRib@users.noreply.github.com>
2024-04-17 08:35:58 +0700

    Update koneksi.php
    Co-Authored-By: popins89 <167153147+popins89@users.noreply.github.com>

--- koneksi.php
+++ koneksi.php
@@ -1,1 +1,11 @@
-koneksi
+<?php 
+	// sesi dimulai
+    session_start();
+
+	// supaya panggil tidak menyertakan mysqli_query($con, sql perintah) lagi, melainkan langsung q();
+	function q($q){
+        $ss=new mysqli("localhost","root","","");
+        return $ss->query($q);
+        $ss->close();
+    }
+?>
