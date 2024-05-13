059e86b070ab491d033f1da0b3330e41af21decf
Thomas <74390441+ThomRib@users.noreply.github.com>
2024-05-13 08:29:11 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -4,7 +4,10 @@
     
     // supaya panggil tidak menyertakan mysqli_query($con, sql perintah) lagi, melainkan langsung q();
     function q($q){
-        $ss=new mysqli("localhost","root","","db_hemat");
+        $ss=new mysqli("localhost",
+		       "root",
+		       "",
+		       "db_hemat");
         return $ss->query($q);
         $ss->close();
     }
