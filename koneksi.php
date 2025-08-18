9327c5ec4700165fe563114b950128f4a56db987
Thomas <74390441+ThomRib@users.noreply.github.com>
2025-08-18 08:27:57 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -2,6 +2,7 @@
     // sesi dimulai
     session_start();
 
+
     // supaya panggil tidak menyertakan mysqli_query($con, sql perintah) lagi, melainkan langsung q();
     function q($q){
         $ss=new mysqli("localhost","root","","db_hemat");
